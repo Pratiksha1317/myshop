51d8dc052bcff734cf76bc9560b56946d8caf15a
pratiksha <partugurav@gmail.com>
2021-06-13 12:47:37 +0530

    add

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mfyc4yurq_m$@4j!1ku-ckak*nc3_8d-tb4p$au1jc%74+4c_c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','myflip.herokuapp.com']
 
 
 # Application definition
